ae1523c4c9fe4bc594cb524f63db317a00f8de04
Souf <soufianmghirat@hotmail.com>
2025-04-27 14:58:04 +0200

    gg

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+    function floor that takes the floor of the float n
+"""
+import math
+
+
+def to_str(n: float) -> str:
+    """
+    function to_str which takes the float n
+    and print the str of the float
+    """
+    return str(n)
