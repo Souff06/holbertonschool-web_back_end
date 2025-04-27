6136f0f281e9a76ac468d03e25b205e8979f783b
Souf <soufianmghirat@hotmail.com>
2025-04-27 14:46:58 +0200

    gg

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import math
+"""
+    function floor that takes the floor of the float a
+"""
+
+
+def floor(n: float) -> int:
+    """
+        floor 2 string
+
+        Parameters:
+            n: float num
+        Returns:
+            int: the floor of a.
+    """
+    return math.floor(n)
