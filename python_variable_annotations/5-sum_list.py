52590ccd1fbcf04809074c073c06d19f56a9fa4c
Souf <soufianmghirat@hotmail.com>
2025-04-27 15:09:34 +0200

    gg

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+     annotate the following variables with the specified values
+"""
+import math
+
+
+def sum_list(input_list: float) -> float:
+    """
+    function sum_list she add the float
+    argument on the list input_list
+    """
+    return sum(input_list)
