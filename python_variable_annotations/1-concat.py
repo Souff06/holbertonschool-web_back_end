3dec899c15ab3f661888a560680a010947e6908e
Souf <soufianmghirat@hotmail.com>
2025-04-27 14:35:04 +0200

    gg

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+    function concat that takes str1 str and str2 str
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+        Concat 2 string
+
+        Parameters:
+            str1 (str): The first string.
+            str2 (str): The second string.
+
+        Returns:
+            str: the concat of str1 and str2.
+    """
+    return str1 + str2
